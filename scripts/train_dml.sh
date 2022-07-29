6c1f38e4e617d799759e68a5224a6c7182bb1781
lm0079 <gh18931@bristol.ac.uk>
2022-07-29 20:44:16 +0100

    Added training shell scripts

---
+++ scripts/train_dml.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+
+# Assumes images are only on Grevy zebras and already cropped.
+export PYTHONPATH="$PYTHONPATH:$(realpath modules/CameraTraps):$(realpath modules/ai4utils)"
+# TODO: Add staged
